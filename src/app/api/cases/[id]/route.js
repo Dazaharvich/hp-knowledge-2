e55e4da06da2c188f00d253f2b64cc8aba6e5802
Dazaharvich <d.azaharvichp@gmail.com>
2024-06-11 19:01:18 -0400

    update cases

--- src/app/api/cases/[id]/route.js
+++ src/app/api/cases/[id]/route.js
@@ -1,13 +1,100 @@
 import { NextResponse } from "next/server";
+import { pool } from "@/libs/mysql";
 
-export function GET() {
-    return NextResponse.json("Obteniendo caso")
-};
+export async function GET(request, { params }) {
+  try {
+    const result = await pool.query("SELECT * FROM cases WHERE id = ?", [
+      params.id,
+    ]);
 
-export function DELETE() {
-    return NextResponse.json("Eliminando caso")
-};
+    if (result.length == 0) {
+      return NextResponse.json(
+        {
+          message: "Caso no encontrado",
+        },
+        {
+          status: 404,
+        }
+      );
+    }
 
-export function PUT() {
-    return NextResponse.json("Actualizando caso")
-};
+    return NextResponse.json(result[0]);
+  } catch (error) {
+    return NextResponse.json(
+      {
+        message: error.message,
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
+
+export async function DELETE(request, { params }) {
+  try {
+    const result = await pool.query("DELETE FROM cases WHERE id = ?", [
+      params.id,
+    ]);
+
+    if (result.affectedRows == 0) {
+      return NextResponse.json(
+        {
+          message: "Caso no encontrado",
+        },
+        {
+          status: 404,
+        }
+      );
+    }
+
+    console.log(result);
+
+    return new Response(null, { status: 204 });
+  } catch (error) {
+    return NextResponse.json(
+      {
+        message: error.message,
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
+
+export async function PUT(request, { params }) {
+  try {
+    const data = await request.json();
+    const result = await pool.query("UPDATE cases SET ? WHERE id = ?", [
+      data,
+      params.id,
+    ]);
+
+    if (result.affectedRows == 0) {
+      return NextResponse.json(
+        {
+          message: "Caso no encontrado",
+        },
+        {
+          status: 404,
+        }
+      );
+    }
+
+    const updatedCase = await pool.query(
+      "SELECT * FROM cases WHERE id = ?"[params.id]
+    );
+
+    return NextResponse.json(updatedCase[0]);
+  } catch (error) {
+    return NextResponse.json(
+      {
+        message: error.message,
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
